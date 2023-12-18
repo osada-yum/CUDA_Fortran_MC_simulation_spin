67db80a74516efb3c5ca2dba5f5ed50d4f72d22c
osada-yum <osada.yum@gmail.com>
2023-12-18 18:44:40 +0900

    Separate options for optimization and debugging.
    	modified:   fpm_run.sh

--- fpm_run.sh
+++ fpm_run.sh
@@ -1,1 +1,3 @@
-fpm --verbose --compiler='nvfortran' --flag='-g -Mbounds -Minfo=accel -gpu=debug -acc -cuda -cudalib=curand' run
+FCFLAGS="-O2 -acc -cuda -cudalib=curand"
+# FCFLAGS="${FCFLAGS} -g -Mbounds -Minfo=accel -gpu=debug"
+fpm --verbose --compiler='nvfortran' --flag="${FCFLAGS}" run
