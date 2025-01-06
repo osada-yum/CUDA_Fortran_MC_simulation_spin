1723de91a1c1ee9edd5cdf7bc98d2f16af316dba
osada-yum <osada.yum@gmail.com>
2025-01-06 19:21:38 +0900

    Add the core script for XY2d.
    	modified:   scripts/fpm_run_xy2d.sh
    	new file:   scripts/fpm_run_xy2d_core.sh

--- scripts/fpm_run_xy2d.sh
+++ scripts/fpm_run_xy2d.sh
@@ -2,40 +2,13 @@ set -x -u -e
 
 FCFLAGS="-O3 -use_fast_math -acc -cuda -cudalib=curand"
 # FCFLAGS="${FCFLAGS} -g -Mbounds -Minfo=accel -gpu=debug"
-nx=1001
+nx=10001
 ny=$((nx - 1))
 mcs=10000
-sample=5000
-kbt=0.95
+sample=1
+kbt=0.895
+iseed=42
+n_skip=0
 
-progfile="./app/xy2d_gpu_relaxation.f90"
-execfile="./bin/xy2d_gpu_relaxation"
-outputfile="data/XY/2D-XY_GPU_x${nx}_y${ny}_mcs${mcs}_sample${sample}_kbt${kbt}_$(date +%Y%m%d_%H%M%S).dat"
-tmpdir="data/tmp"
-mkdir -p "${tmpdir}"
-tmpfile="$(mktemp --tmpdir=${tmpdir})"
-
-sed -i -e "s/nx = .*/nx = ${nx}_int64/" \
-    -e "s/ny = .*/ny = ${ny}_int64/" \
-    -e "s/mcs = .*/mcs = ${mcs}_int32/" \
-    -e "s/tot_sample = .*/tot_sample = ${sample}_int32/" \
-    -e "s/kbt = .*/kbt = ${kbt}_real64/" \
-    "${progfile}"
-
-fpm --verbose --compiler='nvfortran' --flag="${FCFLAGS}" build xy2d_gpu_relaxation
-start=$(date +%s)
-time fpm install xy2d_gpu_relaxation --prefix="." --verbose --compiler='nvfortran' --flag="${FCFLAGS}"
-${execfile} > "${tmpfile}"
-end=$(date +%s)
-cp -v "${tmpfile}" "${outputfile}"
-echo "output >>> '${outputfile}'"
-chmod 400 "${outputfile}"
-
-hour=$( echo "($end - $start) / 3600" | bc)
-minute=$( echo "(($end - $start) % 3600) / 60" | bc)
-second=$( echo "($end - $start) % 60" | bc)
-minute=$(printf "%02d" "${minute}")
-second=$(printf "%02d" "${second}")
-elapsed_time="${hour}h ${minute}m ${second}s"
-#     model,»  size,»     sample,»     mcs,»   kbt, time
-echo "2D-XY_MET_GPU,  ${nx}x${ny} == $((nx*ny)),»  ${sample},   ${mcs},»   ${kbt}, time ${elapsed_time}, ${outputfile}" | tee -a gpu_xy2d.log
+script_dir="scripts"
+source "${script_dir}/fpm_run_xy2d_core.sh"
