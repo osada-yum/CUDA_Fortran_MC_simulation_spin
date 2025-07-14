e0beb21b5ec5197a59cb5e33ff1a9f1fb5dfd793
osada-yum <osada.yum@gmail.com>
2025-07-14 19:16:25 +0900

    For disordered state.
    	modified:   src/output_utilities_m.f90

--- src/output_utilities_m.f90
+++ src/output_utilities_m.f90
@@ -24,26 +24,29 @@ contains
     end do
   end subroutine output_parameters_from_disorder
 
-  subroutine output_abs_parameters_from_disorder(nall, mcs, order_parameter_abs, order_parameter_x, order_parameter_y, autocorrelation)
+  subroutine output_abs_parameters_from_disorder(nall, mcs, order_parameter_abs, order_parameter_xy, autocorrelation)
     integer(int64), intent(in) :: nall
     integer(int32), intent(in) :: mcs
-    type(variance_covariance_kahan), intent(in) :: order_parameter_abs(mcs)
-    type(variance_kahan), intent(in) :: order_parameter_x(mcs), order_parameter_y(mcs), autocorrelation(mcs)
+    type(variance_covariance_kahan), intent(in) :: order_parameter_abs(mcs), order_parameter_xy(mcs)
+    type(variance_kahan), intent(in) :: autocorrelation(mcs)
+    real(real64) :: chi
     integer(int32) :: i
-    write(output_unit, '(a)') "# N, Nsample, time, <|m|>, <e>, <m^2>, <e^2>, N*Var[|m|], N*Var[e], N*Cov[|x|,e], <A>, <A^2>, N*Var[A]"&
-         & //", <m_x>, <m_x^2>, N*Var[mx], <m_y>, <m_y^2>, N*Var[my]"
+    write(output_unit, '(a)') "# N, Nsample, time" // &
+         & ", <|m|>, <e>" // &
+         & ", <m^2>, <e^2>" //&
+         & ", <|m|e>, (<m^2> - (<mx>^2 + <my>^2))" // &
+         & ", <A>, <A^2>" // &
+         & ", <mx>, <my>" // &
+         & ", <mx^2>, <my^2>, <mx*my>"
     do i = 1, mcs
+       chi = order_parameter_abs(i)%square_mean1() - (order_parameter_xy(i)%mean1() ** 2 + order_parameter_xy(i)%mean2() ** 2)
        write(output_unit, '(*(g0, 1x))') nall, order_parameter_abs(i)%num_sample(), i, &
             & order_parameter_abs(i)%mean1(), order_parameter_abs(i)%mean2(), &
             & order_parameter_abs(i)%square_mean1(), order_parameter_abs(i)%square_mean2(), &
-            & nall * order_parameter_abs(i)%var1(), nall * order_parameter_abs(i)%var2(), &
-            & nall * order_parameter_abs(i)%cov(), &
+            & order_parameter_abs(i)%mean_v1v2(), chi, &
             & autocorrelation(i)%mean(), autocorrelation(i)%square_mean(), &
-            & nall * autocorrelation(i)%var(), &
-            & order_parameter_x(i)%mean(), order_parameter_x(i)%square_mean(), &
-            & nall * order_parameter_x(i)%var(), &
-            & order_parameter_y(i)%mean(), order_parameter_y(i)%square_mean(), &
-            & nall * order_parameter_y(i)%var()
+            & order_parameter_xy(i)%mean1(), order_parameter_xy(i)%mean2(), &
+            & order_parameter_xy(i)%square_mean1(), order_parameter_xy(i)%square_mean2(), order_parameter_xy(i)%mean_v1v2()
     end do
   end subroutine output_abs_parameters_from_disorder
 end module output_utilities_m
